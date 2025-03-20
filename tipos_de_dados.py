651be2b93543aac653ad072137e221cd6444ab4f
DeivideSouza <deivide111@gmail.com>
2025-03-20 10:00:49 -0300

    Feat: Testando Variasveis e Constantes

---
+++ tipos_de_dados.py
@@ -0,0 +1,11 @@
+print (20 + 10 + 50)
+print(True)
+print (False)
+print ('python')
+
+# Comentário
+
+idade = 28
+nome = "Deivide Souza"
+
+print(f"Olá meu nome é {nome}, e tenho {idade} anos de idade")
